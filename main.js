92cd56feee966e901602b91965026474a2078058
darkend <hiby.90hou@qq.com>
2018-02-13 14:40:03 +1100

    add enter change color function

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 console.log('work');
 
-setColorButton = document.querySelector('#setColorButton').addEventListener("click",function(){
+function changeColor(){
 	colorName = document.querySelector('#getColorName')
 	console.log(colorName.value);
 	document.querySelector('.brush').style.backgroundColor = colorName.value;
 	event.preventDefault();
+}
+
+setColorButton = document.querySelector('#setColorButton')
+setColorButton.addEventListener("click",changeColor)
+
+document.querySelector('#getColorName').addEventListener("keydown",function(){
+	if(13 == event.keyCode){
+		// alert('browser enter key down');
+		changeColor();
+	}
 })
